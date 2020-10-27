f863a41d39dee1d2c87112577ce1986f3c972fbd
jacobhistorymaker <historymaker777@hotmail.com>
2020-10-27 14:24:45 +0200

    Function nextSequence created for selecting random colours

---
+++ game.js
@@ -0,0 +1,25 @@
+
+// The four colour options
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// Variable storing random color selections, a pattern for the user to follow
+var gamePattern = [];
+
+
+
+
+
+
+
+
+// Random generated color selection from the game
+function nextSequence(){
+  // Create a random number between 0-3
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  // Random selected colour
+  var randomChosenColour = buttonColours[randomNumber];
+
+  // Store the randomChosenColour to the sequence of colors
+  gamePattern.push(randomChosenColour);
+}
